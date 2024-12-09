d4c9da05bb7f942253a0d605932f8f0d0db218e2
Roshan476 <roshanpoudel864@gmail.com>
2024-12-09 14:01:54 +0545

    .env remove and again commit

--- controller/userController.js
+++ controller/userController.js
@@ -37,6 +37,23 @@ export const fetch = async (req, res) => {
     }
 };
 
+//Fetch a user by ID
+export const fetchById = async (req, res) => {
+    try {
+        const id= req.params.id;
+        const userData = await user.findById({ _id:id});
+// check if the user exists 
+        if (!userData) {
+            return res.status(404).json({ message: "User not found." });
+        }
+
+        res.status(200).json(userData);
+    } catch (error) {
+        console.error("Error fetching users:", error);
+        res.status(500).json({ error: "Internal Server error." });
+    }
+};
+
 // Update a user by ID
 export const update = async (req, res) => {
     try {
